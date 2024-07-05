e3520ac950ead4fff1449ff2c9c8bb8e6783120a
V <pvv.1983.minsk@gmail.com>
2024-07-05 13:34:42 +0300

    Изменение ALLOWED_HOSTS

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6zze=wdi-q*po@zy02dnga+=k9phob(&*dw@*^i@8ck#c(%n_x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['valerypalchekh.pythonanywhere.com',]
 
 
 # Application definition
