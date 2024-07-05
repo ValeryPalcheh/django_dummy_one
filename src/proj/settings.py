bfbac45c7927c86a86f844810277fa0fce3357b3
V <pvv.1983.minsk@gmail.com>
2024-07-05 13:42:52 +0300

    Изменение DEBUG

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zze=wdi-q*po@zy02dnga+=k9phob(&*dw@*^i@8ck#c(%n_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['valerypalchekh.pythonanywhere.com',]
 
