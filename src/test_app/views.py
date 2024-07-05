dbcab51b5303ec9c7414d179408c1bac04ce864a
V <pvv.1983.minsk@gmail.com>
2024-07-05 12:23:00 +0300

    Создание test_app

---
+++ src/test_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home_page(request):
+    order_id = request.GET.get("order_id")
+    return HttpResponse(f"<h1>Hello</h1>Your order ID is: {order_id}")
